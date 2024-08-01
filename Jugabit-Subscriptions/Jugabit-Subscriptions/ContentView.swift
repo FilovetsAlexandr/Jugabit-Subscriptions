a38d730aad34dde2c2412c51a64aadb44d0fe7ac
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-08-01 13:26:03 +0300

    initial commit

---
+++ Jugabit-Subscriptions/Jugabit-Subscriptions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jugabit-Subscriptions
+//
+//  Created by Alexandr Filovets on 1.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jugabit-Subscriptions/Jugabit-Subscriptions/Jugabit_SubscriptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jugabit_SubscriptionsApp.swift
+//  Jugabit-Subscriptions
+//
+//  Created by Alexandr Filovets on 1.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Jugabit_SubscriptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
